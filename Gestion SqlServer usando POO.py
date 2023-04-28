926f8f16f09985f7c1c6c92084730f0404ef7872
rootedstone <diegodiazop@gmail.com>
2023-04-28 18:26:56 -0500

    Segundo commit, se modificaron los ejemplos

--- Gestion SqlServer usando POO.py
+++ Gestion SqlServer usando POO.py
@@ -45,8 +45,6 @@ class conexion():
 #con1=conexion('nombre_dsn','nombre_base_de_datos')
 #con1.consulta('select * from nombre_tabla')
 
-# Donde df es el origen(dataframe) y tabla es el destino (tabla sql)
-#con1.carga_tabla12(df=movie,tabla='Movie') 
-#con1.carga_tabla1(pais_df,'Pais')
+
 
 # 
